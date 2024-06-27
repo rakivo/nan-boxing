3b6dd853a782091778171d1a6d6340aedabfc371
rakivo <awfulmacro@gmail.com>
2024-06-28 00:08:53 +0300

    Betta nen boxin

--- src/nan.rs
+++ src/nan.rs
@@ -10,8 +10,33 @@ pub enum Type {
     Ptr,
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct NaNBox(f64);
+#[derive(Copy, Debug, Clone)]
+pub struct NaNBox(pub f64);
+
+impl PartialEq for NaNBox {
+    fn eq(&self, b: &Self) -> bool {
+        let (Ok(typea), Ok(typeb)) = (self.get_type(), b.get_type()) else {
+            return false
+        };
+
+        if typea != typeb {
+            return false
+        }
+
+        match typea {
+            Type::F64 => self == b,
+            Type::I64 => self.as_i64() == b.as_i64(),
+            Type::U64 => self.as_u64() == b.as_u64(),
+            Type::Ptr => self.as_ptr() == b.as_ptr()
+        }
+    }
+}
+
+impl std::fmt::Display for NaNBox {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 impl NaNBox {
     fn mk_inf() -> f64 {
@@ -24,7 +49,7 @@ impl NaNBox {
             Type::F64 => 1,
             Type::I64 => 2,
             Type::U64 => 3,
-            Type::Ptr => 4,
+            Type::Ptr => 4
         };
         let mut bits = x.to_bits();
         bits = (bits & !TYPE_MASK) | (((TYPE_MASK >> 48) & type_val) << 48);
@@ -56,7 +81,7 @@ impl NaNBox {
     }
 
     #[inline(always)]
-    fn get_value(&self) -> i64 {
+    pub fn get_value(&self) -> i64 {
         let bits = self.0.to_bits();
         let value = (bits & VALUE_MASK) as i64;
         if (bits & (1 << 63)) != 0 {
@@ -130,6 +155,15 @@ impl NaNBox {
     pub fn get_f64(&self) -> Option::<f64> {
         if self.is_f64() {
             Some(self.as_f64())
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn get_i64(&self) -> Option::<i64> {
+        if self.is_i64() {
+            Some(self.as_i64())
         } else {
             None
         }

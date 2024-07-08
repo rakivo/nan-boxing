6506195833a1ead4c52d37a5386547a47e0f64f3
rakivo <awfulmacro@gmail.com>
2024-07-08 17:11:23 +0300

    Some minor changes

--- src/nan.rs
+++ src/nan.rs
@@ -2,7 +2,8 @@ const EXP_MASK: u64 = ((1 << 11) - 1) << 52;
 const TYPE_MASK: u64 = ((1 << 4) - 1) << 48;
 const VALUE_MASK: u64 = (1 << 48) - 1;
 
-#[derive(Debug, Clone, PartialEq)]
+#[repr(u8)]
+#[derive(Debug, PartialEq)]
 pub enum Type {
     F64,
     I64,
@@ -10,35 +11,28 @@ pub enum Type {
     Ptr,
 }
 
-#[derive(Copy, Debug, Clone)]
+#[derive(Copy, Clone)]
 pub struct NaNBox(pub f64);
 
-impl PartialEq for NaNBox {
-    fn eq(&self, b: &Self) -> bool {
-        let (Ok(typea), Ok(typeb)) = (self.get_type(), b.get_type()) else {
-            return false
-        };
-
-        if typea != typeb {
-            return false
-        }
-
-        match typea {
-            Type::F64 => self == b,
-            Type::I64 => self.as_i64() == b.as_i64(),
-            Type::U64 => self.as_u64() == b.as_u64(),
-            Type::Ptr => self.as_ptr() == b.as_ptr()
+impl std::fmt::Display for NaNBox {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.get_type() {
+            Ok(Type::F64) => write!(f, "{}", self.as_f64()),
+            Ok(Type::I64) => write!(f, "{}", self.as_i64()),
+            Ok(Type::U64) => write!(f, "{}", self.as_u64()),
+            _ => todo!()
         }
     }
 }
 
-impl std::fmt::Display for NaNBox {
+impl std::fmt::Debug for NaNBox {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        std::fmt::Display::fmt(&self, f)
     }
 }
 
 impl NaNBox {
+    #[inline(always)]
     fn mk_inf() -> f64 {
         f64::from_bits(EXP_MASK)
     }
